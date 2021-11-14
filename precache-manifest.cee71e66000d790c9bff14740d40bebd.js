7ba853bb42864bb716e39ed3a1eb2191b9aee342
kiaplayer <kiaplayer@gmial.com>
2021-11-14 18:14:55 +0300

    c2caa4d

---
+++ precache-manifest.cee71e66000d790c9bff14740d40bebd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "497457d3e1f5d779e725",
+    "url": "/css/app.b38d26c8.css"
+  },
+  {
+    "revision": "6e48b99a63b348aa9fd7f7563b69f0e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5554ad525f55438c5af3",
+    "url": "/js/about.1018e48d.js"
+  },
+  {
+    "revision": "497457d3e1f5d779e725",
+    "url": "/js/app.16f2d5fc.js"
+  },
+  {
+    "revision": "a647dcce7ad6689cf5cf",
+    "url": "/js/chunk-vendors.0184cd92.js"
+  },
+  {
+    "revision": "4b14c64efaf846819b9a229b4193c8b7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
